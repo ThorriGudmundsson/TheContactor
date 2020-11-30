6c9f9bf357e5ea8838db44707f566751af788d39
ThorriGudmundsson <71155098+ThorriGudmundsson@users.noreply.github.com>
2020-11-30 17:03:14 +0000

    Appearance of view cleaned up

--- src/views/Contacts/index.jsx
+++ src/views/Contacts/index.jsx
@@ -3,7 +3,6 @@ import { View, Text, Button } from 'react-native';
 import PropTypes from 'prop-types';
 import data from '../../resources/data.json';
 // import { NavigationEvents } from 'react-navigation';
-// import Toolbar from '../../components/Toolbar';
 import ContactList from '../../components/Contacts/ContactList';
 
 class Contacts extends React.Component {
@@ -14,26 +13,15 @@ class Contacts extends React.Component {
       contacts: data.contacts,
       nav: props.navigation,
     };
-    console.log(this.state.contacts);
   }
 
   render() {
     const { contacts, nav } = this.state;
-    console.log(contacts);
     return (
       <View style={{ flex: 1 }}>
-        <Text> This will be contacts (list) view </Text>
-        <Button
-          title="contact"
-          onPress={() => nav.navigate('Contact')}
-        />
         <Button
           title="New Contact"
           onPress={() => nav.navigate('NewContact')}
-        />
-        <Button
-          title="EditContact"
-          onPress={() => nav.navigate('EditContact')}
         />
 
         <ContactList
