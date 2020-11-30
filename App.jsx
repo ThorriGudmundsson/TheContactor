786f4fed8ad22a8a618338340d7333ac36c682c4
ThorriGudmundsson <71155098+ThorriGudmundsson@users.noreply.github.com>
2020-11-30 11:48:13 +0000

    Config files

--- App.jsx
+++ App.jsx
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +9,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  statusBar: {
+    style: 'auto',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style={styles.statusBar} />
+    </View>
+  );
+}
