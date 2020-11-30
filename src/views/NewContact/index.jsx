4ab7c65aef262e4367bd7291f374ff8b49aa84a3
gudmundursvr <gudmundursvr@gmail.com>
2020-11-30 18:57:03 +0000

    new Contact style

--- src/views/NewContact/index.jsx
+++ src/views/NewContact/index.jsx
@@ -1,20 +1,63 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, TouchableHighlight, TextInput,
+} from 'react-native';
 // import { NavigationEvents } from 'react-navigation';
 // import Toolbar from '../../components/Toolbar';
+import { AntDesign } from '@expo/vector-icons';
+import styles from '../../styles/fields';
+import ic_styles from './styles';
 
 class NewContact extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      name: '',
+      phoneNumber: '',
     };
   }
 
+  genericInputHandler(name, value) {
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { name, phoneNumber } = this.state;
     return (
-      <View style={{ flex: 1 }}>
-        <Text> This will be NewContact view </Text>
+
+
+      <View>
+        
+          <TouchableHighlight
+            onPress={() =>{} }
+            style={ic_styles.cameraButton}
+          >
+            <AntDesign name="camera" style={ic_styles.cameraIcon} />
+          </TouchableHighlight>
+
+
+        <TextInput
+          style={styles.inputfield}
+          placeholder="Name"
+          value={name}
+          onChangeText={(text) => this.genericInputHandler('name', text)}
+        />
+
+        <TextInput
+          style={styles.inputfield}
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChangeText={(text) => this.genericInputHandler('phoneNumber', text)}
+        />
+
+
+        <TouchableHighlight
+          onPress={() =>{} }
+        style={styles.saveButton}
+      >
+        <Text style={styles.saveButtonText}>Save</Text>
+      </TouchableHighlight>
       </View>
     );
   }
---
+++ src/views/NewContact/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+
+  cameraButton:{
+    width: 100,
+    height: 100,
+
+    borderWidth: 10,
+    borderColor: '#6db051',
+    borderRadius: 50,
+    marginTop: 60,
+    marginLeft: 150,
+    marginRight: 150,
+    marginBottom: 60,
+
+
+  },
+
+  cameraIcon:{
+    fontSize: 35,
+    color: '#6db051',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    marginTop: 20,
+  },
+
+});
