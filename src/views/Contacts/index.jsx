9107a42f7e7bacdd031454a29a44d9e8c5a29253
ThorriGudmundsson <71155098+ThorriGudmundsson@users.noreply.github.com>
2020-11-30 21:33:39 +0000

    Merge fix

--- src/views/Contacts/index.jsx
+++ src/views/Contacts/index.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import {
+  View, Text, Button, TextInput,
+} from 'react-native';
 import PropTypes from 'prop-types';
 // import { NavigationEvents } from 'react-navigation';
 import ContactList from '../../components/Contacts/ContactList';
-<<<<<<< HEAD
 import { getAllContacts } from '../../services/contactServices';
-=======
-import data from '../../resources/data.json';
->>>>>>> a733f2081ebf16d2b2d9635c32c23f5e3e840803
 
 class Contacts extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      contacts: data.Contacts,
+      contacts: [],
       nav: props.navigation,
     };
   }
@@ -34,16 +32,9 @@ class Contacts extends React.Component {
           title="New Contact"
           onPress={() => nav.navigate('NewContact')}
         />
-<<<<<<< HEAD
-        <Button
-          title="EditContact"
-          onPress={() => nav.navigate('EditContact')}
-        />
-=======
-        <View style={{height: 50, justifyContent: "center"}}>
-          <TextInput placeholder="Search"></TextInput>
+        <View style={{ height: 50, justifyContent: 'center' }}>
+          <TextInput placeholder="Search" />
         </View>
->>>>>>> 35bdf63aec70ac97c5893c978f8489651c5ebb66
         <ContactList
           contacts={contacts}
         />
