288fd33ada1cf83315e59348c14923cde707a3c3
gudmundursvr <gudmundursvr@gmail.com>
2020-12-03 12:30:00 +0000

    segrete delete

--- src/views/secrettests/index.jsx
+++ src/views/secrettests/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
-import { writeSomething, killit } from '../../services/testServices';
+import { writeSomething, killit, testsomthing } from '../../services/testServices';
 // import { NavigationEvents } from 'react-navigation';
 // import Toolbar from '../../components/Toolbar';
 import styles from '../../styles/fields';
@@ -38,8 +38,16 @@ class secrettests extends React.Component {
           onPress={killit}
           style={styles.saveButton}
         >
-        <Text style={styles.saveButtonText}>Kill directory</Text>
-       </TouchableHighlight>
+          <Text style={styles.saveButtonText}>Kill directory</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          onPress={testsomthing}
+          style={styles.saveButton}
+        >
+          <Text style={styles.saveButtonText}>Write something</Text>
+        </TouchableHighlight>
+
       </View>
 
     );
